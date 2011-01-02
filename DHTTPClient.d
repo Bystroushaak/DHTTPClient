06c520c78d2c931131b1904add19dc6992875f43
Lishaak Bystroushaak <bystrousak@kitakitsune.org>
2011-01-02 03:22:07 +0100

    Přidány nějaké definice proměnných.

---
+++ DHTTPClient.d
@@ -0,0 +1,14 @@
+// odstranit
+
+import std.stdio;
+
+// //--
+
+import std.socket;
+
+const auto CLRF = "\r\n";
+const auto HTTP_VERSION = "HTTP/4.1";
+
+void main(){
+    writeln(HTTP_VERSION);
+}
