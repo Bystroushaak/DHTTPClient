5f3befa6d47e281d35228c4545332090e694ef40
Lishaak Bystroushaak <bystrousak@kitakitsune.org>
2011-01-11 17:09:10 +0100

    Dopsána podpora přesměrování, vyjímek. Přidal jsem unittest u ParsedURL a hlavičky pro IE, FF a LFF.

--- DHTTPClient.d
+++ DHTTPClient.d
@@ -1,14 +1,5 @@
-/* DHTTPClient.d v0.3.0 (06.01.2011) by Bystroushaak (bystrousak@kitakitsune.org)
+/* DHTTPClient.d v0.5.0 (11.01.2011) by Bystroushaak (bystrousak@kitakitsune.org)
  * 
- * TODO:
- *  Hledat Location v hlavičkách.
- *      Udělat fci která ignoruje location?
- *  Číst data jako byte a v případě příznivých hlaviček je teprve konvertovat na string.
- * 
- *  Přidělat fce:
- *      + nějaký sety, který jen přidaj další hlavičku
- *      + reakce na 301
- *      + unittest ParsedUrl
 */
 
 import std.uri;
@@ -17,19 +8,63 @@ import std.string;
 import std.socket;
 import std.socketstream;
 
-class HTTPClientException:Exception{
+private enum RequestType {NONEYET, GET, POST, GETANDPOST};
+
+public enum string[string] FFHeaders =  [
+    "User-Agent"      : "Mozilla/5.0 (Windows; U; Windows NT 5.1; cs; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.13",
+    "Accept"          : "text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain",
+    "Accept-Language" : "cs,en-us;q=0.7,en;q=0.3",
+    "Accept-Charset"  : "utf-8",
+    "Keep-Alive"      : "300",
+    "Connection"      : "keep-alive"
+];
+
+public enum string[string] LFFHeaders =  [
+    "User-Agent"      : "Mozilla/5.0 (X11; U; Linux i686; cs; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.13",
+    "Accept"          : "text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain",
+    "Accept-Language" : "cs,en-us;q=0.7,en;q=0.3",
+    "Accept-Charset"  : "utf-8",
+    "Keep-Alive"      : "300",
+    "Connection"      : "keep-alive"
+];
+
+public enum string[string] IEHeaders =  [
+    "User-Agent"      : "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
+    "Accept"          : "text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain",
+    "Accept-Language" : "cs,en-us;q=0.7,en;q=0.3",
+    "Accept-Charset"  : "utf-8",
+    "Keep-Alive"      : "300",
+    "Connection"      : "keep-alive"
+];
+
+public enum string[string] DefaultHeaders = FFHeaders;
+
+
+
+public class HTTPClientException:Exception{
     this(string msg){
         super(msg);
     }
 }
-class URLException:HTTPClientException{
+public class URLException:HTTPClientException{
     this(string msg){
         super(msg);
     }
 }
-class InvalidStateException:HTTPClientException{
+public class InvalidStateException:HTTPClientException{
     this(string msg){
         super(msg);
+    }
+}
+public class StatusCodeException:HTTPClientException{
+    private uint status_code;
+    this(string msg, uint status_code){
+        super(msg);
+        this.status_code = status_code;
+    }
+    
+    uint getStatusCode(){
+        return this.status_code;
     }
 }
 
@@ -56,6 +91,11 @@ private class ParsedURL {
             
             this.domain = t[0];
             this.path   = "/" ~ join(t[1 .. $], "/");
+        }else if (URL.indexOf("?") >= 0){
+            t = split(URL, "?");
+            
+            this.domain = t[0];
+            this.path   = "?" ~ join(t[1 .. $], "?");
         }else{
             this.domain = URL;
             this.path   = "/";
@@ -115,33 +155,60 @@ private class ParsedURL {
     }
 }
 
+unittest{
+    ParsedURL pu = new ParsedURL("http://kitakitsune.org/");
+    
+    assert(pu.getPort() == 80);
+    assert(pu.getPath() == "/");
+    assert(pu.getProtocol() == "http");
+    assert(pu.getDomain() == "kitakitsune.org");
+    
+    pu = new ParsedURL("http://kitakitsune.org?asd");
+    assert(pu.getDomain() == "kitakitsune.org");
+    assert(pu.getPath() == "?asd");
+    
+    pu = new ParsedURL("http://kitakitsune.org:2011?asd");
+    assert(pu.getDomain() == "kitakitsune.org");
+    assert(pu.getPort() == 2011);
+    assert(pu.getPath() == "?asd");
+}
+
+
+
 public class HTTPClient{
     private const string CLRF = "\r\n";
     private const string HTTP_VERSION = "HTTP/1.1";
     private string[string] serverHeaders;
     private string[string] clientHeaders;
     private bool initiated = false;
-    private bool ignore_location = false;
+
+    // This is for exceptions handling
+    private bool ignore_redirect = false;
+    private uint max_recursion = 8;
+    private uint recursion;
+    private RequestType request_type = RequestType.NONEYET;
+    
+    // 301 Redirection handling
+    private string[string] get_params, post_params;
+    
+    this(){
+        this.clientHeaders = cast(string[string]) DefaultHeaders;
+    }
     
     private SocketStream initConnection(ref ParsedURL pu){
         if (pu.getProtocol() != "http"){
             throw new URLException("Bad protocol!");
         }
         
-        setDefaultHeaders();
+        TcpSocket tsock;
         
-        TcpSocket tsock = new TcpSocket(new InternetAddress(pu.getDomain(), pu.getPort()));
-
+        try{
+            tsock = new TcpSocket(new InternetAddress(pu.getDomain(), pu.getPort()));
+        }catch(std.socket.AddressException e){
+            throw new URLException(e.toString());
+        }
+        
         return new SocketStream(tsock);
-    }
-    
-    private void setDefaultHeaders(){
-        this.clientHeaders["User-Agent"] = "Mozilla/5.0 (X11; U; Linux i686; cs; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3";
-        this.clientHeaders["Accept"] = "text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain";
-        this.clientHeaders["Accept-Language"] = "cs,en-us;q=0.7,en;q=0.3";
-        this.clientHeaders["Accept-Charset"]  = "utf-8";
-        this.clientHeaders["Keep-Alive"] = "300";
-        this.clientHeaders["Connection"] =  "keep-alive";
     }
     
     private void sendHeaders(ref SocketStream ss){
@@ -262,7 +329,7 @@ public class HTTPClient{
         string ostr = URL;
         
         // Append url with ?, & and headers..
-        if (!data.length){
+        if (data.length){
             if (ostr.count("?")){
                 if (ostr.count("&")){
                     if (ostr.endsWith("&"))
@@ -283,32 +350,83 @@ public class HTTPClient{
         return ostr;
     }
     
-    public string get(string URL, string[string] params = ["":""]){  
+    private string handleExceptions(string data){
+        // Exceptions handling
+        if ("StatusCode" in this.serverHeaders && !this.serverHeaders["StatusCode"].startsWith("200")){
+            // React on 301 StatusCode (redirection)
+            if (this.serverHeaders["StatusCode"].startsWith("301")){
+                // Check if redirection is allowed
+                if (! this.ignore_redirect){
+                    // Be carefull how many redirections was allready did
+                    if (this.recursion++ <= this.max_recursion){
+                        final switch(this.request_type){
+                            case RequestType.GET:
+                                return this.get(this.serverHeaders["Location"], this.get_params);
+                            case RequestType.POST:
+                                return this.post(this.serverHeaders["Location"], this.post_params);
+                            case RequestType.GETANDPOST:
+                                return this.getAndPost(this.serverHeaders["Location"], this.get_params, this.post_params);
+                            case RequestType.NONEYET:
+                                throw new HTTPClientException("This is pretty strange exception - code flow _NEVER_ shoud be here!"); 
+                                break;
+                        }
+                    }else{
+                        this.recursion = 0;
+                        throw new HTTPClientException("Error - too many (" ~ std.conv.to!(string)(this.max_recursion) ~ ") redirections.");
+                    }
+                }else{ // If redirection isn't allowed, return page with redirection headers
+                    this.recursion = 0;
+                    return data;
+                }
+            }else{ // Every other StatusCode throwing exception
+                throw new StatusCodeException(this.serverHeaders["StatusCode"], to!(uint)(this.serverHeaders["StatusCode"][0 .. 3]));
+            }
+        }else{ // StatusCode 200 - Ok
+            this.recursion = 0;
+            return data;
+        }   
+    }
+    
+    public string get(string URL, string[string] params = ["":""]){
+        // Save status for case of redirection
+        this.request_type = RequestType.GET;
+        this.get_params = params;
+        
+        // Parse URL
         ParsedURL pu = new ParsedURL(this.parseGetURL(URL, params));
         
         // Initialize connection
         SocketStream ss = initConnection(pu);
         
-        // Write GET request TODO: přidat možnost odeslat GET data, přidat odeslání vlastních hlaviček
+        // Write GET request
         ss.writeString("GET " ~ pu.getPath() ~ " " ~ HTTP_VERSION ~ CLRF);
         ss.writeString("Host: " ~ pu.getDomain() ~ CLRF);
         this.sendHeaders(ss);
         ss.writeString(CLRF);
         
         ss.flush();
-
-        // Read everything and close connection
-        return readHeadersAndBody(ss);
+        
+        // Read everything and close connection, handle exceptions
+        return handleExceptions(readHeadersAndBody(ss));
     }
     
     public string post(string URL, string[string] params = ["":""]){
+        // Save status for case of redirection
+        if (this.request_type != RequestType.GETANDPOST){
+            this.request_type = RequestType.POST;
+            this.post_params = params;
+        }
+        
+        // Parse URL
         ParsedURL pu = new ParsedURL(URL);
+        
+        // Encode params
         string enc_params = this.urlEncodeHeaders(params);
         
         // Initialize connection
         SocketStream ss = initConnection(pu);
 
-        // Write GET request TODO: přidat možnost odeslat GET data, přidat odeslání vlastních hlaviček
+        // Write GET request
         ss.writeString("POST " ~ pu.getPath() ~ " " ~ HTTP_VERSION ~ CLRF);
         ss.writeString("Host: " ~ pu.getDomain() ~ CLRF);
         this.sendHeaders(ss);
@@ -322,11 +440,16 @@ public class HTTPClient{
         
         ss.flush();
 
-        // Read everything and close connection
-        return readHeadersAndBody(ss);
+        // Read everything and close connection, handle exceptions
+        return handleExceptions(readHeadersAndBody(ss));
     }
     
     public string getAndPost(string URL, string[string] get, string[string] post){
+        // Save status for case of redirection
+        this.request_type = RequestType.GETANDPOST;
+        this.get_params = get;
+        this.post_params = post;
+        
         return this.post(parseGetURL(URL, get), post);
     }
     
@@ -350,6 +473,20 @@ public class HTTPClient{
         
         this.clientHeaders = fheaders;
     }
+    
+    public bool getIgnoreRedirect(){
+        return this.ignore_redirect;
+    }
+    public void setIgnoreRedirect(bool ir){
+        this.ignore_redirect = ir;
+    }
+    
+    public uint getMaxRecursion(){
+        return this.max_recursion;
+    }
+    public void setMaxRecursion(uint mr){
+        this.max_recursion = mr;
+    }
 }
 
 
@@ -358,27 +495,25 @@ debug{
     import std.stdio;
     
     void main(){
-        //~ string URL = "http://kitakitsune.org/";
-        //~ string URL = "http://kitakitsune.org/proc/time.php"; // one simple line with date
-        //~ string URL = "http://kitakitsune.org/bhole/parametry.php";
-        string URL = "http://bit.ly/ebi4js"; // redirect
-        //~ string URL = "http://anoncheck.security-portal.cz";
-        //~ string URL = "http://anoncheck.security-portal.cz/background.gif";
-        //~ string URL = "http://martiner.blogspot.com/2010/09/muj-nejdrazsi.html"; // not exactly normal response from server..
-        //~ string URL = "http://janucesenka.blbne.cz/21848-komentare.html";
+//         string URL = "http://kitakitsune.org/";
+//         string URL = "http://kitakitsune.org/proc/time.php"; // one simple line with date
+//         string URL = "http://kitakitsune.org/bhole/parametry.php";
+//         string URL = "http://bit.ly/gX0v4M"; // redirect
+//         string URL = "http://anoncheck.security-portal.cz";
+//         string URL = "http://anoncheck.security-portal.cz/background.gif";
+//         string URL = "http://martiner.blogspot.com/2010/09/muj-nejdrazsi.html"; // not exactly normal response from server..
+//         string URL = "http://janucesenka.blbne.cz/21848-komentare.html";
         
-        
-        
-        //~ try{
-        //~ writeln(cl.get(URL));
-        
-        //~ 
-        //~ string[string] post = ["typ dat":"post", "postkey":"postval.."];
-        //~ string[string] get = ["typ dat":"get", "getkey":"getval.."];
+        string[string] post = ["typ dat":"post", "postkey":"postval.."];
+        string[string] get = ["typ dat":"get", "getkey":"getval.."];
         HTTPClient cl = new HTTPClient();
-        writeln(cl.get(URL));
-        writeln(cl.getResponseHeaders());
+//         writeln(cl.get(URL, get));
+//         writeln(cl.post(URL, post));
+        cl.setIgnoreRedirect(true);
+        writeln(cl.getClientHeaders());
+//         writeln(cl.getAndPost("http://kitakitsune.org/xa", get, post));
+//         writeln(cl.getResponseHeaders());
 
-        //~ std.file.write("asd.gif", cl.get(URL));
+//         std.file.write("asd.gif", cl.get(URL));
     }
 }
