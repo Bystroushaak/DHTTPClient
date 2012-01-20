0b6dd29386dd5c8dec38232d3e4ba038c37f29a7
Bystroushaak <bystrousak@kitakitsune.org>
2012-01-20 18:07:20 +0100

    Updated examples/download_binary_file.d

--- examples/download_binary_file.d
+++ examples/download_binary_file.d
@@ -1,3 +1,14 @@
+/**
+ * Binary download example
+ * 
+ * Author:  Bystroushaak (bystrousak@kitakitsune.org)
+ * Version: 0.0.1
+ * Date:    20.01.2012
+ * 
+ * Copyright: 
+ *     This work is licensed under a CC BY.
+ *     http://creativecommons.org/licenses/by/3.0/
+*/
 import std.stdio;
 import dhttpclient;
 
@@ -7,6 +18,9 @@ int main(string[] args){
 	
 	// binary files are easy, you just have to cast them to ubyte[]
 	std.file.write("logo3w.png", cast(ubyte[]) cl.get("http://www.google.cz/images/srpr/logo3w.png"));
+	
+	// mimetype of downloaded file
+	writeln(cl.getResponseHeaders()["Content-Type"]);
 	
 	return 0;
 }
